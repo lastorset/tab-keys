7479f24117a4345ae807433b077c26e90c7fc70b
Leif Arne Storset <leifarne@storset.net>
2015-11-20 22:30:50 +0100

    Clone a tab with Alt+C

---
+++ js/background.js
@@ -0,0 +1,13 @@
+chrome.commands.onCommand.addListener(function(command) {
+	if (command == 'clone-tab') {
+        chrome.tabs.query({
+            active: true,
+            // TODO: Will this work correctly?
+            currentWindow: true
+        }, function(tabs) {
+            console.assert(tabs.length == 1, "Unexpected tab count: "+ tabs.length);
+
+            chrome.tabs.duplicate(tabs[0].id);
+        });
+	}
+});
